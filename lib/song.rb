21af629b248474613bfe6c8175ae853583fbaf84
Ishani <ishaniamin@gmail.com>
2018-06-15 19:40:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,17 +44,17 @@ class Song
     return artist_hash
   end
 
-  def name
-    @name
-  end
-
-  def artist
-    @artist
-  end
-
-  def genre
-    @genre
-  end
+  # def name
+  #   @name
+  # end
+  #
+  # def artist
+  #   @artist
+  # end
+  #
+  # def genre
+  #   @genre
+  # end
 
 
 end
