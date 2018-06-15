361239292a3f5dc4b0c6dfa8fc55111af3561d25
Ishani <ishaniamin@gmail.com>
2018-06-15 19:34:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,19 @@ class Song
 
   attr_accessor :name :artist :genre
 
+  @@count = 0
+
   def initialize
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
   end
+
+  def count
+    @@count
+  end
+
+  def genres
 
 end
